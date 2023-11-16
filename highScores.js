5197a8a4e860c19064db46920d834226d25dbff4
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2023-11-16 14:15:00 +0100

    Update highScores.js

--- highScores.js
+++ highScores.js
@@ -10,30 +10,17 @@ function getXmlHttp() {
 }
 
 function highScoresOnLoad() {
-    // div called id=highScoreDiv
-    var xmlhttp = getXmlHttp();
-    xmlhttp.onreadystatechange=function()
-    {
-	if (xmlhttp.readyState==4 && xmlhttp.status==200)
-	{
-	    var response = jsonParse(xmlhttp.responseText),
-	    dailyScoreList = response.dailyScores,
-	    dailyOutput,
-	    i;
+	dailyOutput,
+	i;
 
-	    dailyOutput= '<table class="highScoreTable"><tr class="highScoreTableHeader"><td>#</td><td>Name</td><td>Score</td></tr>';
-
-	    for (i = 0; i < dailyScoreList.length; i += 1) {
-		curScore = dailyScoreList[i];
-		dailyOutput += '<tr><td>' + (i+1) + '</td><td>' + curScore.name + '</td><td>' + curScore.score + '</td></tr>';
-	    }
-
-	    dailyOutput += '</table>';
-
-	    document.getElementById("dailyScoreDiv").innerHTML = dailyOutput;
+	dailyOutput= '<table class="highScoreTable"><tr class="highScoreTableHeader"><td>#</td><td>Name</td><td>Score</td></tr>';
+	dailyScoreList = JSON.parse(localstorage.highscore)
+	for (i = 0; i < dailyScoreList.length; i += 1) {
+	curScore = dailyScoreList[i];
+	dailyOutput += '<tr><td>' + (i+1) + '</td><td>' + curScore + '</td></tr>';
 	}
-    }
 
-    xmlhttp.open("POST", "/score/tables", true);
-    xmlhttp.send();
-}
+	dailyOutput += '</table>';
+
+	document.getElementById("dailyScoreDiv").innerHTML = dailyOutput;
+
